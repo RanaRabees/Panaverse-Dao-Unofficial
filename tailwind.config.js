632ae73426f2a0a485ddba770d444f4dcc6f5b7f
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2023-05-17 05:47:09 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        bk: "#09121d",
+        wt: "#F5F5F5",
+        t2: "#2e2e3f",
+        accent: "#11AD8E",
+      },
+      fontFamily: {
+        out: ["var(--font-outfit)"],
+      },
+      color: {
+        blacka: "#000000",
+        blackb: "#111111",
+        blackc: "#222222"
+      }
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
